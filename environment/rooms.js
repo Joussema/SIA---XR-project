5344699c37f744ec8d9df505ab739db3dc32e204
HiggsCorleone <j.oussema2002@gmail.com>
2026-01-16 01:04:38 +0100

    minor fix

--- environment/rooms.js
+++ environment/rooms.js
@@ -192,14 +192,14 @@ export const rooms = [
       {
         id: 'start',
         // Standard room entrance
-        position: [0, 2, -2],
-        forward: [0, 0, -1],
+        position: [0, 2, 2], // Fixed: -2 allows correct alignment after 180 deg rotation
+        forward: [0, 0, 1],   // Fixed: 1 Forces 180 deg rotation against buffer start (1)
       },
       {
         id: 'end',
         // Standard room exit
         position: [0, 20, -20],
-        forward: [0, 0, 1],
+        forward: [0, 0, -1],
       },
     ],
   },
@@ -212,12 +212,12 @@ export const rooms = [
       {
         id: 'start',
         position: [10, 2, 16],
-        forward: [0, 0, 1],
+        forward: [0, 0, -1],
       },
       {
         id: 'end',
         position: [-10, 2, -16],
-        forward: [0, 0, -1],
+        forward: [0, 0, 1],
       },
     ],
   },
